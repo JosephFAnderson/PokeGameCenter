f1225f18fabbcf16f3d33465183432abcffd1689
cjtavares <105406233+cjtavares@users.noreply.github.com>
2022-09-28 12:56:01 -0400

    working of function to fill pokedex data

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,17 @@
-var getPokemon = function() {fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+var getPokemon = function(pokeName) {
+  var poke = "Pikachu".toLowerCase();
+  fetch("https://pokeapi.co/api/v2/pokemon/" + poke + "/")
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
     var pokeID = data.id;
-    console.log(pokeID)
+    var name = data.name;
+    console.log(name);
+    var nameHeader = document.querySelector("#pokename");
+    console.log(nameHeader)
+    nameHeader.textContent = name;
     var idSearch = "https://pokeapi.co/api/v2/pokemon-species/" + pokeID + "/"
     fetch(idSearch)
   .then(function (response) {
@@ -13,6 +19,10 @@ var getPokemon = function() {fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
   })
   .then(function (data) {
     console.log(data);
+    var pokeEntry = data.flavor_text_entries[0].flavor_text;
+    console.log(pokeEntry)
+    var dexEntry = document.querySelector("#pokeentry")
+    dexEntry.textContent = pokeEntry
   });
   });
 }
