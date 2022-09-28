702e4ed9974762638a6eb8c2178f2385f0729953
Joseph A <joseph_anderson11@yahoo.com>
2022-09-28 18:05:39 -0400

    created img carousel

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var getPokemon = function(pokeName) {
+var getPokemon = function(pokeName) {    
   var poke = "Pikachu".toLowerCase();
   fetch("https://pokeapi.co/api/v2/pokemon/" + poke)
   .then(function (response) {
@@ -27,6 +27,54 @@ var getPokemon = function(pokeName) {
   });
   });
 }
+
+// Take in the users pokemon name and fetch the TCG cards for that pokemon
+function getTCG(pokeName) {
+    // Create variable to add header to fetch call
+    var options = {
+	method: 'GET',
+	headers: {
+		'X-API-Key': '2d0d487d-d62c-4cbd-a2a0-fe8c74e6ce13'
+    }};
+
+    // Fetch pokemone cards from Pokemotcg API
+    fetch('https://api.pokemontcg.io/v2/cards?q=name:' + pokeName, options)
+	    .then(function (response) {
+            return response.json()
+        } )
+	    .then(function (data) {
+            // Create Array containing the img objects
+            var pokeImg = data.data
+            var carouselList = document.querySelector('.carousel-indicators');
+            var innerCarousel = document.querySelector('.carousel-inner');           
+
+            // Iterate through the array. Creating carousel elements and adding iamges to them
+            for(var i = 0; i < pokeImg.length; i++) {               
+                var carouselLi = document.createElement('li');
+                carouselLi.setAttribute('data-type', '#carouselExampleIndicators');
+                carouselLi.setAttribute('data-slide-to', i);
+                carouselList.appendChild(carouselLi);
+
+                var singleCarDiv = document.createElement('div');
+                singleCarDiv.classList.add('carousel-item');
+                
+                // Set first carousel image as active
+                if(i === 0) {
+                    singleCarDiv.classList.add('active');
+                }
+
+                var img = document.createElement('img');
+                img.setAttribute('src', pokeImg[i].images.small);
+                img.classList.add('d-block');
+                img.classList.add('w-100');
+
+                singleCarDiv.appendChild(img);
+                innerCarousel.appendChild(singleCarDiv);                
+            }
+        })
+}
+
+
 
 var searchButton = document.getElementById("searchButton");
 
