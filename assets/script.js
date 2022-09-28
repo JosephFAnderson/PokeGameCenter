2cb18233cdad741cdca23a9162df2b6ecbc7e9c1
cod3burrito <robintanner44@gmail.com>
2022-09-28 14:37:06 -0400

    fixing my booboo

--- assets/script.js
+++ assets/script.js
@@ -32,34 +32,4 @@ var searchButton = document.getElementById("searchButton");
 
 searchButton.addEventListener("click", searchPoke);
 
-function searchPoke(event){
-    event.preventDefault();
-    var initialSearch = document.getElementById("searcher");
-    var pokeName = initialSearch.value;
-    initialSearch.value = "";
-    getPokemon(pokeName);
-}
-var pokemonVGC = function(gameInfo) {
-    var game = "Console Games"();
-    fetch("https://pokeapi.co/api/v2/version-group/" + name + "/")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-      var gameNames = data.version_groups;
-      fetch("https://pokeapi.co/api/v2/move-target/" + name + "/")  
-      .then (this(response.json();))
-      var pokeType = function(getType) {
-        fetch("https://pokeapi.co/api/v2/type/" + name + "/")
-      .then(this(response.json();))
-      };
-      var betterThan = function(superEffective) {
-        fetch("https://pokeapi.co/api/v2/type/" + name + "/")
-        .then (this(response.json){double_damage_to};))
-      };
-      var weakerThan = function(callNurseJoy) {
-        fetch("https://pokeapi.co/api/v2/type/" + name + "/")
-        .then (this(response.json){double_damage_from};))
-      };
 
-vgc.onclick = function(gameInfo);
